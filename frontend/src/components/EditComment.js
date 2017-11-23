ed78da2241505b73da7a6b392c7008b02cbc16d5
unknown <tino.hertlein@googlemail.com>
2017-11-23 19:51:38 +0100

    refactor: move input controls into methods

--- frontend/src/components/EditComment.js
+++ frontend/src/components/EditComment.js
@@ -9,29 +9,23 @@ class EditComment extends Component {
       this.state = {
         title: props.entity.title,
         body: props.entity.body,
-        category: props.entity.category ? props.entity.category : props.categories[0].name,
+        category: props.entity.category ? props.entity.category : (props.categories ? props.categories[0].name : null),
         author: props.entity.author,
-        id: props.entity.id,
-        timestamp: props.entity.timestamp
+        id: props.entity.id
       }
 
       this.handleInputChange = this.handleInputChange.bind(this)
       this.handleSubmit = this.handleSubmit.bind(this)
     }
 
-
   handleInputChange(event) {
-    const target = event.target
-    const name = target.name
-
     this.setState({
-      [name]: target.value
+      [event.target.name]: event.target.value
     })
   }
 
   handleSubmit(event) {
     event.preventDefault()
-
     console.log(JSON.stringify(this.state))
     this.props.onSubmit()
   }
@@ -40,26 +34,49 @@ class EditComment extends Component {
     return this.props.entityType === EntityType.Post
   }
 
-  getAdditionaPostInputControls = () => {
-    return (
-      <div>
-        <FormGroup>
-          <ControlLabel>Title</ControlLabel>
-          <FormControl
-            name='title'
-            type='text'
-            defaultValue={this.state.title}
-            onChange={this.handleInputChange}>
-          </FormControl>
-        </FormGroup>
-        <FormGroup>
-          <ControlLabel>Select Category</ControlLabel>
-          <FormControl name='category' onChange={this.handleInputChange} componentClass='select' defaultValue={this.state.category}>
-            {this.props.categories.map((category) => (<option key={category.name} value={category.name}>{category.name}</option>))}
-          </FormControl>
-        </FormGroup>
-      </div>
-    )
+  getTitleInput = () => {
+    return this.showAdditionaPostInputControls() && (<FormGroup>
+      <ControlLabel>Title</ControlLabel>
+      <FormControl
+        name='title'
+        type='text'
+        defaultValue={this.state.title}
+        onChange={this.handleInputChange}>
+      </FormControl>
+    </FormGroup>)
+  }
+
+  getBodyInput = () => {
+    return <FormGroup>
+      <ControlLabel>Body</ControlLabel>
+      <FormControl
+        type='text'
+        name='body'
+        componentClass='textarea'
+        defaultValue={this.state.body}
+        onChange={this.handleInputChange}
+      />
+    </FormGroup>
+  }
+
+  getCategoryInput = () => {
+    return this.showAdditionaPostInputControls() && (<FormGroup>
+      <ControlLabel>Select Category</ControlLabel>
+      <FormControl name='category' onChange={this.handleInputChange} componentClass='select' defaultValue={this.state.category}>
+        {this.props.categories.map((category) => (<option key={category.name} value={category.name}>{category.name}</option>))}
+      </FormControl>
+    </FormGroup>)
+  }
+
+  getAuthorInput = () => {
+  return  <FormGroup>
+    <ControlLabel>Author</ControlLabel>
+    <FormControl
+      type='textarea'
+      name='author'
+      defaultValue={this.state.author}
+      onChange={this.handleInputChange}/>
+  </FormGroup>
   }
 
   render() {
@@ -70,37 +87,14 @@ class EditComment extends Component {
             <Modal.Title>{actionType.name} {entityType.name}</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            {this.showAdditionaPostInputControls() && this.getAdditionaPostInputControls() }
-            <FormGroup>
-              <ControlLabel>Body</ControlLabel>
-              <FormControl
-                type='text'
-                name='body'
-                componentClass='textarea'
-                defaultValue={this.state.body}
-                onChange={this.handleInputChange}
-              />
-            </FormGroup>
-            <FormGroup>
-              <ControlLabel>Author</ControlLabel>
-              <FormControl
-                type='textarea'
-                name='author'
-                defaultValue={this.state.author}
-                onChange={this.handleInputChange}/>
-            </FormGroup>
+            {this.getTitleInput()}
+            {this.getBodyInput()}
+            {this.getCategoryInput()}
+            {this.getAuthorInput()}
           </Modal.Body>
           <Modal.Footer>
-            <Button
-              onClick={onCancel}>
-              Cancel
-            </Button>
-            <Button
-              bsStyle='primary'
-              onClick={this.handleSubmit}
-            type="submit">
-              Submit
-            </Button>
+            <Button onClick={onCancel}> Cancel</Button>
+            <Button bsStyle='primary'onClick={this.handleSubmit} type="submit">Submit</Button>
           </Modal.Footer>
         </Modal.Dialog>
       )
