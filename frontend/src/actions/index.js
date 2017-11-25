b53fd639463277b71234b8a59d01f192143ad9c1
unknown <tino.hertlein@googlemail.com>
2017-11-25 16:53:03 +0100

    feat: create redux-actions

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -1,6 +1,15 @@
 export const CREATE_POST = 'CREATE_POST'
+export const DELETE_POST = 'DELETE_POST'
+export const INC_POST_VOTE = 'INC_POST_VOTE'
+export const DEC_POST_VOTE = 'DEC_POST_VOTE'
 
-export function createPost ({ id, title, body, author, category }) {
+export const CREATE_COMMENT = 'CREATE_COMMENT'
+export const DELETE_COMMENT = 'DELETE_COMMENT'
+export const INC_COMMENT_VOTE = 'INC_COMMENT_VOTE'
+export const DEC_COMMENT_VOTE = 'DEC_COMMENT_VOTE'
+
+
+export createPost = ({ id, title, body, author, category }) => {
   return {
     type: CREATE_POST,
     id,
@@ -8,5 +17,59 @@ export function createPost ({ id, title, body, author, category }) {
     body,
     author,
     category
+  }
+}
+
+export deletePost = (postId) => {
+  return {
+    type: DELETE_POST,
+    postId
+  }
+}
+
+export incPostVote = (postId) => {
+  return {
+    type: INC_POST_VOTE,
+    postId
+  }
+}
+
+export decPostVote = (postId) => {
+  return {
+    type: DEC_POST_VOTE,
+    postId
+  }
+}
+
+export createComment = ({ id, postId, body, author }) => {
+  return {
+    type: CREATE_POST,
+    id,
+    postId,
+    title,
+    body,
+    author,
+    category
+  }
+}
+
+export deleteComment = (commentId) => {
+  return {
+    type: DELETE_COMMENT,
+    commentId
+  }
+}
+
+export incCommentVote = (commentId) => {
+  return {
+    type: INC_COMMENT_VOTE,
+    commentId
+  }
+}
+
+export decCommentVote = (commentId) => {
+  return {
+    type: DEC_COMMENT_VOTE,
+    commentId
   }
 }
